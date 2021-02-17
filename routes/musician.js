7fd77de8bea6127a9d0a2f739d3ebe108a4a7ef6
manavsethi <manavbliss@gmail.com>
2021-02-16 22:11:41 -0500

    neww

--- routes/musician.js
+++ routes/musician.js
@@ -6,7 +6,7 @@ const schema = require('../store/schema');
 
 // healthcheck
 router.get('/health', (req, res) => {
-  res.status('200').send("Status: okji!");
+  res.status('200').send("Status: okji!!!");
 });
 
 // retrieve all musicians from data store
